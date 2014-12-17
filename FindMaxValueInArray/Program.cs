f9a2705d4f0598215bb274fba90ec8927eca9555
Frank J Castro <frankjtc@gmail.com>
2014-12-17 15:19:13 -0800

    fourth commit

---
+++ FindMaxValueInArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxValueInArray
+{
+    class FindMaxVal
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 1,34,56,233,7,666,23};
+            int maxVal = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (maxVal < myArray[i])
+                {
+                    maxVal = myArray[i];
+                }
+            }
+
+            Console.WriteLine(maxVal.ToString());
+            Console.ReadLine();
+        }
+    }
+}
